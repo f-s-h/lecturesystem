8cc6f24055946f26e6df1decd2d420f02810cf1b
f-s-h <fabian.heinrich@tum.de>
2023-03-10 11:05:56 +0100

    Set up GradeRepository.java

---
+++ src/main/java/com/fsh/lecturesystem/repository/GradeRepository.java
@@ -0,0 +1,7 @@
+package com.fsh.lecturesystem.repository;
+
+import com.fsh.lecturesystem.entity.GradeEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface GradeRepository extends CrudRepository<GradeEntity, Long> {
+}
