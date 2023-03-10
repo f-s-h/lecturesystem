f705ad3551d76fe6a25f1880efe654e84ab749c3
f-s-h <fabian.heinrich@tum.de>
2023-03-10 10:49:29 +0100

    Set up LocationEntity.java

---
+++ src/main/java/com/fsh/lecturesystem/entity/LocationEntity.java
@@ -0,0 +1,27 @@
+package com.fsh.lecturesystem.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="LOCATIONS")
+@NoArgsConstructor
+@Getter
+@Setter
+public class LocationEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "location_generator")
+    @SequenceGenerator(name = "location_generator", sequenceName = "location_seq", allocationSize = 1)
+    private Long locationId;
+    private String room;
+    private String building;
+    private String address;
+
+    public LocationEntity(String room, String building, String address) {
+        this.room = room;
+        this.building = building;
+        this.address = address;
+    }
+}
