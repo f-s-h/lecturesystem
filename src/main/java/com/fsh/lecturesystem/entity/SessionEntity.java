e0bd9b005bb1daaee0b72198308126687e9750c6
f-s-h <fabian.heinrich@tum.de>
2023-03-10 09:58:18 +0100

    Set up of SessionEntity.java

---
+++ src/main/java/com/fsh/lecturesystem/entity/SessionEntity.java
@@ -0,0 +1,23 @@
+package com.fsh.lecturesystem.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="SESSIONS")
+@NoArgsConstructor
+@Getter
+@Setter
+public class SessionEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "session_generator")
+    @SequenceGenerator(name="session_generator", sequenceName = "session_seq", allocationSize = 1)
+    private Long userId;
+    private String session;
+
+    public SessionEntity(String session){
+        this.session = session;
+    }
+}
