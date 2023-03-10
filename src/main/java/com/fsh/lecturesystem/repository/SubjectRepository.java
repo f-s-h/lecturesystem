9682058a481b3b01e6c1bbb1aab41908accba61e
f-s-h <fabian.heinrich@tum.de>
2023-03-10 11:06:35 +0100

    Set up SubjectRepository.java

---
+++ src/main/java/com/fsh/lecturesystem/repository/SubjectRepository.java
@@ -0,0 +1,7 @@
+package com.fsh.lecturesystem.repository;
+
+import com.fsh.lecturesystem.entity.SubjectEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SubjectRepository extends CrudRepository<SubjectEntity, Long> {
+}
